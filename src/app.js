e9fed247522886b1fef483e1d035e44149918ce4
DESKTOP-F7V67DT\cuong <domanhcuong250701@gmail.com>
2023-10-15 15:38:03 +0700

    add jsonwebtoken

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-require('module-alias/register');
-const express = require('express');
-const connect_db = require('./config/connect-db');
+require("module-alias/register");
+const express = require("express");
+const connect_db = require("./config/connect-db");
 
 const app = express();
 
 connect_db();
 
-app.use('/', require('@routes'));
+app.use("/", require("@routes"));
 
 module.exports = app;
--- src/config/env.js
+++ src/config/env.js
@@ -1,33 +1,38 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const env = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
-    port: process.env.DB_PORT
-}
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
+  port: process.env.DB_PORT,
+};
 
 const input_connnection = {
-    host: env.host,
-    port: env.port,
-    dialect: 'mysql',
-    query: { raw: true },
-    pool: {
-        max: 30,
-        min: 0,
-        acquire: 60000,
-        idle: 5000,
-    },
-    define: {
-        hooks: true,
-    },
-    timezone: "+07:00",
-    logging: true,
-}
+  host: env.host,
+  port: env.port,
+  dialect: "mysql",
+  query: { raw: true },
+  pool: {
+    max: 30,
+    min: 0,
+    acquire: 60000,
+    idle: 5000,
+  },
+  define: {
+    hooks: true,
+  },
+  timezone: "+07:00",
+  // logging: true,
+};
 
-const sequelize = new Sequelize(env.database, env.user, env.password, input_connnection);
+const sequelize = new Sequelize(
+  env.database,
+  env.user,
+  env.password,
+  input_connnection
+);
 
 module.exports = sequelize;
