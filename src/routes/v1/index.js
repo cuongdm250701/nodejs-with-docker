a951eae3020356e5147e5e4e24ce2a2d34cd7f78
cuong.domanh <cuong.domanh@vti.com.vn>
2023-10-25 15:30:52 +0700

    test routing on ec2 instance

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -7,6 +7,30 @@ router.get("/", (req, res, next) => {
   res.json({ message: "Welcome to api v1" });
 });
 
+// test router
+
+router.get("/users", (req, res, next) => {
+  const users = [
+    {
+      id: 1,
+      name: "cuongdm",
+    },
+    {
+      id: 2,
+      name: "cuongdm1",
+    },
+    {
+      id: 3,
+      name: "cuongdm2",
+    },
+    {
+      id: 4,
+      name: "cuongdm3",
+    },
+  ];
+  res.json({ message: "Successfully", data: users });
+});
+
 router.use("/users", require("@routes/v1/user.route"));
 router.use("/category-posts", require("@routes/v1/category-post.route"));
 router.use("/posts", require("@routes/v1/post.route"));
