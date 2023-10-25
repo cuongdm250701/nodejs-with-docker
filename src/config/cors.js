380e4f90430bcaf94aaf8da128fefcc128b0d2a6
cuong.domanh <cuong.domanh@vti.com.vn>
2023-10-25 15:12:36 +0700

    config allow access ec2 instance

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 const options = {
-  origin: ["10.1.38.116:3001", "https://www.google.com.vn"],
+  origin: ["10.1.38.116:3001", "https://www.google.com.vn", "54.169.93.145"],
   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
 };
 
